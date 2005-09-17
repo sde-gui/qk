69a0765a5c7ac892e1b75d78a71745fd760c96f0
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2005-09-17 00:39:10 +0000

    moo_bind_property()

--- moo/mooutils/bind.c
+++ moo/mooutils/bind.c
@@ -14,76 +14,6 @@
 #include "mooutils/bind.h"
 
 
-typedef struct {
-    GtkWidget  *wid;
-    guint       invert : 1;
-} WBPair;
-
-
-static void button_destroyed (GArray *array)
-{
-    if (array)
-        g_array_free (array, TRUE);
-}
-
-
-static void button_toggled (GtkToggleButton *btn)
-{
-    gboolean active = gtk_toggle_button_get_active (btn);
-    GArray *dep_ctls = (GArray*) g_object_get_data (G_OBJECT (btn), "moo_bind_sensitive_ctls");
-    guint i;
-
-    if (!dep_ctls) return;
-    for (i = 0; i < dep_ctls->len; ++i) {
-        WBPair *pair = &g_array_index (dep_ctls, WBPair, i);
-        gtk_widget_set_sensitive (pair->wid, pair->invert ? !active : active);
-    }
-}
-
-
-void        moo_bind_sensitive  (GtkToggleButton    *btn,
-                                 GtkWidget         **dependent,
-                                 int                 num,
-                                 gboolean            invert)
-{
-    GArray *dep_ctls;
-    int i;
-    GtkWidget **w;
-
-    g_return_if_fail (btn != NULL && dependent != NULL);
-    if (num == 0) return;
-
-    dep_ctls = (GArray*) g_object_get_data (G_OBJECT (btn), "moo_bind_sensitive_ctls");
-    if (!dep_ctls) {
-        dep_ctls = g_array_new (FALSE, FALSE, sizeof(WBPair));
-        g_object_set_data_full (G_OBJECT (btn), "moo_bind_sensitive_ctls",
-                                dep_ctls, (GDestroyNotify) button_destroyed);
-    }
-
-    if (num > 0)
-        for (i = 0; i < num; ++i)
-        {
-            WBPair p = {dependent[i], invert};
-            g_array_append_val (dep_ctls, p);
-        }
-    else
-        for (w = dependent; *w != NULL; ++w)
-        {
-            WBPair p = {*w, invert};
-            g_array_append_val (dep_ctls, p);
-        }
-
-    button_toggled (btn);
-
-    g_signal_handlers_disconnect_matched (btn, G_SIGNAL_MATCH_FUNC,
-                                          0, 0, 0, (gpointer)button_toggled, 0);
-    g_signal_connect (G_OBJECT (btn), "toggled", G_CALLBACK (button_toggled), NULL);
-}
-
-
-/*****************************************************************************/
-
-
 static void button_clicked (GtkButton *btn, GtkEntry *entry)
 {
     MooQueryTextFunc func = (MooQueryTextFunc)g_object_get_data (G_OBJECT (btn), "moo_query_text_func");
@@ -121,3 +51,219 @@ char       *moo_quote_text      (const char *text,
     return text ? g_strdup_printf ("\"%s\"", text) : NULL;
 }
 
+
+/****************************************************************************/
+/* BindProperty
+ */
+
+typedef struct {
+    GObject *source;
+    GObject *target;
+    GParamSpec *source_prop;
+    GParamSpec *target_prop;
+    gboolean alive;
+    MooTransformPropFunc transform;
+    gpointer data;
+    GDestroyNotify destroy_notify;
+} PropWatcher;
+
+
+static void source_died         (PropWatcher *watcher);
+static void target_died         (PropWatcher *watcher);
+static void watcher_check       (PropWatcher *watcher);
+static void watcher_die         (PropWatcher *watcher);
+
+
+static PropWatcher*
+prop_watcher_new (GObject            *target,
+                  const char         *target_prop,
+                  GObject            *source,
+                  const char         *source_prop,
+                  MooTransformPropFunc transform,
+                  gpointer            transform_data,
+                  GDestroyNotify      destroy_notify)
+{
+    PropWatcher *watcher;
+    GObjectClass *target_class, *source_class;
+    char *signal_name;
+
+    g_return_val_if_fail (G_IS_OBJECT (target), NULL);
+    g_return_val_if_fail (G_IS_OBJECT (source), NULL);
+    g_return_val_if_fail (target_prop != NULL, NULL);
+    g_return_val_if_fail (source_prop != NULL, NULL);
+    g_return_val_if_fail (transform != NULL, NULL);
+
+    target_class = g_type_class_peek (G_OBJECT_TYPE (target));
+    source_class = g_type_class_peek (G_OBJECT_TYPE (source));
+
+    watcher = g_new0 (PropWatcher, 1);
+
+    watcher->source = source;
+    watcher->target = target;
+
+    watcher->source_prop = g_object_class_find_property (source_class, source_prop);
+    if (!watcher->source_prop)
+    {
+        g_warning ("%s: could not find property '%s' in class '%s'",
+                   G_STRLOC, source_prop, g_type_name (G_OBJECT_TYPE (source)));
+        g_free (watcher);
+        return NULL;
+    }
+
+    watcher->target_prop = g_object_class_find_property (target_class, target_prop);
+    if (!watcher->target_prop)
+    {
+        g_warning ("%s: could not find property '%s' in class '%s'",
+                   G_STRLOC, target_prop, g_type_name (G_OBJECT_TYPE (target)));
+        g_free (watcher);
+        return NULL;
+    }
+
+    watcher->alive = TRUE;
+
+    watcher->transform = transform;
+    watcher->data = transform_data;
+    watcher->destroy_notify = destroy_notify;
+
+    signal_name = g_strdup_printf ("notify::%s", source_prop);
+    g_signal_connect_swapped (source, signal_name,
+                              G_CALLBACK (watcher_check), watcher);
+    g_object_weak_ref (source, (GWeakNotify) source_died, watcher);
+    g_object_weak_ref (target, (GWeakNotify) target_died, watcher);
+
+    g_free (signal_name);
+    return watcher;
+}
+
+
+static void
+watcher_die (PropWatcher *watcher)
+{
+    if (watcher->source)
+    {
+        g_signal_handlers_disconnect_by_func (watcher->source,
+                                              (gpointer) watcher_check,
+                                              watcher);
+        g_object_weak_unref (watcher->source, (GWeakNotify) source_died, watcher);
+    }
+
+    if (watcher->target)
+        g_object_weak_unref (watcher->target, (GWeakNotify) target_died, watcher);
+
+    if (watcher->destroy_notify)
+        watcher->destroy_notify (watcher->data);
+
+    g_free (watcher);
+}
+
+
+void
+moo_bind_property_full (GObject            *target,
+                        const char         *target_prop,
+                        GObject            *source,
+                        const char         *source_prop,
+                        MooTransformPropFunc transform,
+                        gpointer            transform_data,
+                        GDestroyNotify      destroy_notify)
+{
+    PropWatcher *watcher;
+
+    g_return_if_fail (G_IS_OBJECT (target));
+    g_return_if_fail (G_IS_OBJECT (source));
+    g_return_if_fail (target_prop != NULL);
+    g_return_if_fail (source_prop != NULL);
+    g_return_if_fail (transform != NULL);
+
+    watcher = prop_watcher_new (target, target_prop,
+                                source, source_prop,
+                                transform, transform_data,
+                                destroy_notify);
+
+    if (!watcher)
+        return;
+
+    watcher_check (watcher);
+}
+
+
+static void
+source_died (PropWatcher *watcher)
+{
+    watcher->source = NULL;
+    watcher_die (watcher);
+}
+
+
+static void
+target_died (PropWatcher *watcher)
+{
+    watcher->target = NULL;
+    watcher_die (watcher);
+}
+
+
+static void
+watcher_check (PropWatcher *watcher)
+{
+    GValue source_val, target_val;
+
+    if (!G_IS_OBJECT (watcher->source) ||
+         !G_IS_OBJECT (watcher->target))
+    {
+        watcher_die (watcher);
+        g_return_if_reached ();
+    }
+
+    source_val.g_type = 0;
+    target_val.g_type = 0;
+
+    g_value_init (&source_val, watcher->source_prop->value_type);
+    g_value_init (&target_val, watcher->target_prop->value_type);
+
+    g_object_get_property (watcher->source,
+                           watcher->source_prop->name,
+                           &source_val);
+    watcher->transform (&target_val, &source_val, watcher->data);
+    g_object_set_property (watcher->target,
+                           watcher->target_prop->name,
+                           &target_val);
+
+    g_value_unset (&source_val);
+    g_value_unset (&target_val);
+}
+
+
+static void
+bool_transform (GValue        *target,
+                const GValue  *source,
+                gpointer       invert)
+{
+    if (invert)
+        g_value_set_boolean (target, !g_value_get_boolean (source));
+    else
+        g_value_set_boolean (target, g_value_get_boolean (source));
+}
+
+void
+moo_bind_bool_property (GObject            *target,
+                        const char         *target_prop,
+                        GObject            *source,
+                        const char         *source_prop,
+                        gboolean            invert)
+{
+    moo_bind_property_full (target, target_prop, source, source_prop,
+                            bool_transform, GINT_TO_POINTER (invert), NULL);
+}
+
+
+void
+moo_bind_sensitive (GtkToggleButton    *btn,
+                    GtkWidget         **dependent,
+                    int                 num_dependent,
+                    gboolean            invert)
+{
+    int i;
+    for (i = 0; i < num_dependent; ++i)
+        moo_bind_bool_property (G_OBJECT (dependent[i]), "sensitive",
+                                G_OBJECT (btn), "active", invert);
+}
--- moo/mooutils/bind.h
+++ moo/mooutils/bind.h
@@ -23,18 +23,36 @@ G_BEGIN_DECLS
 typedef const char  *(*MooQueryTextFunc)        (gpointer data, GtkWidget *button);
 typedef char        *(*MooTransformTextFunc)    (const char *text, gpointer data);
 
-char       *moo_quote_text      (const char *text, gpointer data);
+char       *moo_quote_text          (const char *text, gpointer data);
 
-void        moo_bind_button     (GtkButton                  *button,
-                                 GtkEntry                   *entry,
-                                 MooQueryTextFunc            func,
-                                 MooTransformTextFunc        text_func,
-                                 gpointer                    data);
+void        moo_bind_button         (GtkButton          *button,
+                                     GtkEntry           *entry,
+                                     MooQueryTextFunc    func,
+                                     MooTransformTextFunc text_func,
+                                     gpointer            data);
 
-void        moo_bind_sensitive  (GtkToggleButton    *btn,
-                                 GtkWidget         **dependent,
-                                 int                 num_dependent,
-                                 gboolean            invert);
+void        moo_bind_sensitive      (GtkToggleButton    *btn,
+                                     GtkWidget         **dependent,
+                                     int                 num_dependent,
+                                     gboolean            invert);
+
+void        moo_bind_bool_property  (GObject            *target,
+                                     const char         *target_prop,
+                                     GObject            *source,
+                                     const char         *source_prop,
+                                     gboolean            invert);
+
+typedef void (*MooTransformPropFunc)(GValue             *target,
+                                     const GValue       *source,
+                                     gpointer            data);
+
+void        moo_bind_property       (GObject            *target,
+                                     const char         *target_prop,
+                                     GObject            *source,
+                                     const char         *source_prop,
+                                     MooTransformPropFunc transform,
+                                     gpointer            transform_data,
+                                     GDestroyNotify      destroy_notify);
 
 
 G_END_DECLS
