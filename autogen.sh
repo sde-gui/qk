a92b72e1af61f7c68a541c32439b0f4015d630ac
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2005-11-03 22:56:53 +0000

    Empty changelog

--- autogen.sh
+++ autogen.sh
@@ -5,57 +5,33 @@ test -z "$srcdir" && srcdir=.
 
 echo "Generating configuration files..."
 
-if test x$AUTOMAKE_VERSION != x; then
-    am_version=-$AUTOMAKE_VERSION
-fi
-if test x$AUTOCONF_VERSION != x; then
-    ac_version=-$AUTOCONF_VERSION
-fi
-if test x$LIBTOOL_VERSION != x; then
-    lt_version=-$LIBTOOL_VERSION
-fi
-
-if test x$ACLOCAL = x; then
-    export ACLOCAL=aclocal$am_version
-fi
-if test x$AUTOMAKE = x; then
-    export AUTOMAKE=automake$am_version
-fi
-
-if test x$AUTOCONF = x; then
-    export AUTOCONF=autoconf$ac_version
-fi
-if test x$AUTOHEADER = x; then
-    export AUTOHEADER=autoheader$ac_version
-fi
-if test x$AUTOM4TE = x; then
-    export AUTOM4TE=autom4te$ac_version
-fi
-
-if test x$LIBTOOL = x; then
-    export LIBTOOL=libtool$lt_version
-fi
-if test x$LIBTOOLIZE = x; then
-    export LIBTOOLIZE=libtoolize$lt_version
-fi
-
 if test -d $srcdir/m4 ; then
     aclocal_extra="-I $srcdir/m4"
 fi
 
-echo $LIBTOOLIZE --automake --copy      && \
-$LIBTOOLIZE --automake --copy           && \
-                                           \
-echo $ACLOCAL $aclocal_extra            && \
-$ACLOCAL $aclocal_extra                 && \
-                                           \
-echo $AUTOHEADER                        && \
-$AUTOHEADER                             && \
-                                           \
-echo $AUTOMAKE --add-missing --copy     && \
-$AUTOMAKE --add-missing --copy          && \
-                                           \
-echo $AUTOCONF                          && \
-$AUTOCONF                               && \
-                                           \
-echo && echo "run './configure ; make ; make install'" && echo
+ACLOCAL=${ACLOCAL:-aclocal-1.9}
+AUTOMAKE=${AUTOMAKE:-automake-1.9}
+
+LIBTOOLIZE=${LIBTOOLIZE:-libtoolize}
+
+AUTOHEADER=${AUTOHEADER:-autoheader}
+AUTOCONF=${AUTOCONF:-autoconf}
+
+echo $LIBTOOLIZE --automake --copy
+$LIBTOOLIZE --automake --copy || exit $?
+
+echo $ACLOCAL $ACLOCAL_FLAGS $aclocal_extra
+$ACLOCAL $ACLOCAL_FLAGS $aclocal_extra || exit $?
+
+echo $AUTOHEADER
+$AUTOHEADER || exit $?
+
+echo $AUTOMAKE --add-missing --copy
+$AUTOMAKE --add-missing --copy || exit $?
+
+echo $AUTOCONF
+$AUTOCONF || exit $?
+
+echo
+echo "run './configure ; make ; make install'"
+echo
