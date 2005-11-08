611a54f7623656adfb2befc816d72d77fae04cc7
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2005-11-08 19:00:32 +0000

    Added python console

--- moo/moopython/plugins/pystuff.py
+++ moo/moopython/plugins/pystuff.py
@@ -1,5 +1,12 @@
 import moo
 import gtk
+import pango
+
+try:
+    import pyconsole
+    have_pyconsole = True
+except ImportError:
+    have_pyconsole = False
 
 PLUGIN_ID = "PyStuff"
 
@@ -18,23 +25,50 @@ class Plugin(object):
     def init(self):
         editor = moo.edit.editor_instance()
         xml = editor.get_ui_xml()
-        moo.utils.window_class_add_action (moo.edit.EditWindow, "ShowPythonConsole",
-                                           name="Show Python Console",
-                                           label="Show Python Console",
-                                           callback=self.show_console)
+        moo.utils.window_class_add_action (moo.edit.EditWindow, "ShowLogWindow",
+                                           name="Show Log Window",
+                                           label="Show Log Window",
+                                           callback=self.show_log_window)
         self.ui_merge_id = xml.new_merge_id()
         xml.add_item(self.ui_merge_id, "Editor/Menubar/Tools",
-                     "ShowPythonConsole", "ShowPythonConsole", -1)
+                     "ShowLogWindow", "ShowLogWindow", -1)
+
+        if have_pyconsole:
+            moo.utils.window_class_add_action (moo.edit.EditWindow, "ShowPythonConsole",
+                                               name="Show Python Console",
+                                               label="Show Python Console",
+                                               callback=self.show_console)
+            xml.add_item(self.ui_merge_id, "Editor/Menubar/Tools",
+                        "ShowPythonConsole", "ShowPythonConsole", -1)
         return True
 
     def deinit(self):
         editor = moo.edit.editor_instance()
         xml = editor.get_ui_xml()
+        moo.utils.window_class_remove_action(moo.edit.EditWindow, "ShowLogWindow");
         moo.utils.window_class_remove_action(moo.edit.EditWindow, "ShowPythonConsole");
         xml.remove_ui(self.ui_merge_id)
 
-    def show_console(self, window):
+    def show_log_window(self, window):
         moo.app.get_instance().show_python_console()
 
+    def show_console(self, window):
+        window = gtk.Window()
+        swin = gtk.ScrolledWindow()
+        swin.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_ALWAYS)
+        window.add(swin)
+
+        console_type = pyconsole.ConsoleType(moo.edit.TextView)
+        console = console_type(use_rlcompleter=False)
+        console.set_property("highlight-current-line", False)
+        editor = moo.edit.editor_instance()
+        mgr = editor.get_lang_mgr()
+        lang = mgr.get_lang("PythonConsole")
+        console.set_lang(lang)
+        console.modify_font(pango.FontDescription("Courier New 11"))
+
+        swin.add(console)
+        swin.set_size_request(400,300)
+        window.show_all()
 
 moo.edit.plugin_register(Plugin)
