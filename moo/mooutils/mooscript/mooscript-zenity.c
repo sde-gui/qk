5bee7cfb0f3afd267be9caf398850f5bef2ade56
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2006-02-26 03:31:29 -0600

    Simple dialogs for scripts, ala zenity

---
+++ moo/mooutils/mooscript/mooscript-zenity.c
@@ -0,0 +1,78 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4; coding: utf-8 -*-
+ *
+ *   mooscript-zenity.c
+ *
+ *   Copyright (C) 2004-2006 by Yevgen Muntyan <muntyan@math.tamu.edu>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   See COPYING file that comes with this distribution.
+ */
+
+#include "mooscript-zenity.h"
+#include "mooscript-context.h"
+#include <gtk/gtk.h>
+
+
+static MSValue*
+entry_func (MSValue   **args,
+            guint       n_args,
+            MSContext  *ctx)
+{
+    char *dialog_text = NULL, *entry_text = NULL;
+    gboolean hide_text = FALSE;
+    int response;
+    GtkWidget *dialog, *entry;
+    MSValue *result;
+
+    if (n_args > 0)
+        dialog_text = ms_value_print (args[0]);
+    if (n_args > 1)
+        entry_text = ms_value_print (args[1]);
+    if (n_args > 2)
+        hide_text = ms_value_get_bool (args[2]);
+
+    dialog = gtk_dialog_new_with_buttons (NULL, ctx->window,
+                                          GTK_DIALOG_MODAL | GTK_DIALOG_NO_SEPARATOR,
+                                          GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+                                          GTK_STOCK_OK, GTK_RESPONSE_OK,
+                                          NULL);
+    gtk_dialog_set_default_response (GTK_DIALOG (dialog), GTK_RESPONSE_OK);
+
+    if (dialog_text)
+    {
+        GtkWidget *label;
+        label = gtk_label_new (dialog_text);
+        gtk_widget_show (label);
+        gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dialog)->vbox), label, FALSE, FALSE, 0);
+    }
+
+    entry = gtk_entry_new ();
+    gtk_widget_show (entry);
+    gtk_entry_set_visibility (GTK_ENTRY (entry), !hide_text);
+    gtk_entry_set_text (GTK_ENTRY (entry), entry_text ? entry_text : "");
+    gtk_entry_set_activates_default (GTK_ENTRY (entry), TRUE);
+    gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dialog)->vbox), entry, FALSE, FALSE, 0);
+
+    response = gtk_dialog_run (GTK_DIALOG (dialog));
+
+    if (response == GTK_RESPONSE_OK)
+        result = ms_value_string (gtk_entry_get_text (GTK_ENTRY (entry)));
+    else
+        result = ms_value_none ();
+
+    gtk_widget_destroy (dialog);
+    g_free (dialog_text);
+    g_free (entry_text);
+
+    return result;
+}
+
+MSFunc *
+ms_zenity_entry (void)
+{
+    return ms_cfunc_new_var (entry_func);
+}
---
+++ moo/mooutils/mooscript/mooscript-zenity.h
@@ -0,0 +1,29 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4; coding: utf-8 -*-
+ *
+ *   mooscript-zenity.h
+ *
+ *   Copyright (C) 2004-2006 by Yevgen Muntyan <muntyan@math.tamu.edu>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   See COPYING file that comes with this distribution.
+ */
+
+#ifndef __MOO_SCRIPT_ZENITY_H__
+#define __MOO_SCRIPT_ZENITY_H__
+
+#include "mooscript-func.h"
+
+G_BEGIN_DECLS
+
+
+/* Entry(dialog_text, entry_text, hide_text) */
+MSFunc  *ms_zenity_entry    (void);
+
+
+G_END_DECLS
+
+#endif /* __MOO_SCRIPT_ZENITY_H__ */
