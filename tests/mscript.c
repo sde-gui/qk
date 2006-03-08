32d3665ac71fa8fd476455be8c8e2758b67f2558
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2006-03-08 02:19:44 -0600

    Interactive interpreter

--- tests/mscript.c
+++ tests/mscript.c
@@ -15,12 +15,69 @@
 #include <gtk/gtk.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 
 static void usage (const char *prg)
 {
     g_print ("usage:\t%s <file>\n\t%s -c <script>\n", prg, prg);
     exit (1);
+}
+
+
+static int run_interactive (void)
+{
+    MSContext *ctx;
+
+    using_history ();
+
+    ctx = ms_context_new ();
+
+    while (TRUE)
+    {
+        char *line;
+        MSNode *node;
+        MSValue *val;
+
+        line = readline (">>> ");
+
+        if (!line)
+        {
+            g_print ("\n");
+            return 0;
+        }
+
+        node = ms_script_parse (line);
+        add_history (line);
+        free (line);
+
+        if (!node)
+        {
+            g_print ("syntax error\n");
+            continue;
+        }
+
+        val = ms_top_node_eval (node, ctx);
+        ms_node_unref (node);
+
+        if (!val)
+        {
+            g_print ("%s\n", ms_context_get_error_msg (ctx));
+            ms_context_clear_error (ctx);
+            continue;
+        }
+
+        if (!ms_value_is_none (val))
+        {
+            char *s = ms_value_print (val);
+            g_print ("%s\n", s);
+            g_free (s);
+        }
+
+        ms_value_unref (val);
+    }
 }
 
 
@@ -32,11 +89,11 @@ int main (int argc, char *argv[])
     MSValue *val;
     MSContext *ctx;
 
-//    gtk_init (&argc, &argv);
     g_type_init ();
+    ms_type_init ();
 
     if (argc < 2)
-        usage (argv[0]);
+        return run_interactive ();
 
     if (argc == 2)
         file = argv[1];
