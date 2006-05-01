088e875e507fc42c2d3e95466f5694572daee86a
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2006-04-30 20:56:03 -0500

    Do *not* change value when setting new default, it breaks values loaded from file

--- moo/mooutils/mooprefs.c
+++ moo/mooutils/mooprefs.c
@@ -582,11 +582,7 @@ item_set_default (PrefsItem      *item,
 
     if (!moo_value_equal (value, &item->default_value))
     {
-        if (moo_value_equal (&item->default_value, &item->value))
-            g_value_copy (value, &item->value);
-
         g_value_copy (value, &item->default_value);
-
         return TRUE;
     }
 
