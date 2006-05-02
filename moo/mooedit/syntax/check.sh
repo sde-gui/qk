37a2656c68a413373730bc1deaa43508ef4ad9de
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2006-05-02 07:53:37 -0500

    Added gnuplot lang file

--- moo/mooedit/syntax/check.sh
+++ moo/mooedit/syntax/check.sh
@@ -4,7 +4,7 @@ langs="c.lang diff.lang latex.lang perl.lang \
 misc.lang make.lang ms.lang python.lang \
 python-console.lang sh.lang xml.lang \
 sci.lang gap.lang scheme.lang texinfo.lang \
-changelog.lang"
+changelog.lang diff.lang gnuplot.lang"
 
 styles="garnacho.styles gvim.styles"
 files="$langs $styles"
