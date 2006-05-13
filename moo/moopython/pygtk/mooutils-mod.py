4551cece0ae7021fca56e2257cb419184a5d0bee
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2006-05-13 16:06:10 -0500

    Use gobject.new if available

--- moo/moopython/pygtk/mooutils-mod.py
+++ moo/moopython/pygtk/mooutils-mod.py
@@ -1,36 +1,54 @@
 """moo.utils module"""
 import _moo_utils as _utils
 import gtk as _gtk
+import gobject as _gobject
 from _moo_utils import *
 
+
+def _activate(action, callback, window):
+    callback(window)
 
 class ActionFactory(object):
     def __init__(self, action_id, **kwargs):
         object.__init__(self)
         self.id = action_id
-        self.kwargs = kwargs
+
+        self.props = {}
+        self.fake_props = {}
+
+        for key in kwargs.keys():
+            if key in ["callback", "display_name", "accel", "no_accel"]:
+                self.fake_props[key] = kwargs[key]
+            else:
+                self.props[key] = kwargs[key]
 
     def __call__(self, window):
         self.window = window
-        action = _gtk.Action(self.id, None, None, None)
-        self.set_action_properties(action)
+        if _gtk.check_version(2, 8, 0):
+            action = _gtk.Action(self.id, None, None, None)
+            self.set_props(action)
+        else:
+            action = _gobject.new(_gtk.Action, name=self.id, **self.props)
+        self.set_fake_props(action)
         return action
 
-    def set_action_properties(self, action):
-        for key in self.kwargs.keys():
+    def set_fake_props(self, action):
+        for key in self.fake_props.keys():
             if key == "callback":
-                action.connect("activate", self.action_activate)
+                action.connect("activate", _activate, self.fake_props[key], self.window)
             elif key == "display_name":
-                action_set_display_name(action, self.kwargs[key])
+                action_set_display_name(action, self.fake_props[key])
             elif key == "accel":
-                action_set_default_accel(action, self.kwargs[key])
+                action_set_default_accel(action, self.fake_props[key])
             elif key == "no_accel":
-                action_set_no_accel(action, self.kwargs[key])
+                action_set_no_accel(action, self.fake_props[key])
             else:
-                action.set_property(key, self.kwargs[key])
+                raise ValueError("unknown property " + key)
 
-    def action_activate(self, action):
-        self.kwargs["callback"](self.window)
+    def set_props(self, action):
+        for key in self.props.keys():
+            action.set_property(key, self.props[key])
+
 
 
 def window_class_add_action(klass, action_id, **kwargs):
