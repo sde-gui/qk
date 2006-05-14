003cef5b3891afd844e63b72094be5555c5b8eab
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2006-05-14 18:45:37 -0500

    Output strings as char[], not char*

--- moo/mooutils/xml2h.sh
+++ moo/mooutils/xml2h.sh
@@ -6,6 +6,6 @@ fi
 VARNAME=$1
 INPUT=$2
 
-echo "static const char *$VARNAME = \"\""
+echo "static const char $VARNAME [] = \"\""
 sed 's/"/\\"/g' $INPUT | sed 's/^\(.*\)$/"\1\\n"/' || exit $?
 echo ";"
