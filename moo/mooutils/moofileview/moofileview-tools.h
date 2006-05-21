7b16724d4e10d647882ba3002e9bfbeba8e55f69
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2006-05-21 11:17:14 -0500

    Gonna start Open With thing

---
+++ moo/mooutils/moofileview/moofileview-tools.h
@@ -0,0 +1,25 @@
+/*
+ *   moofileview-tools.h
+ *
+ *   Copyright (C) 2004-2006 by Yevgen Muntyan <muntyan@math.tamu.edu>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   See COPYING file that comes with this distribution.
+ */
+
+#ifndef __MOO_FILE_VIEW_TOOLS_H__
+#define __MOO_FILE_VIEW_TOOLS_H__
+
+#include "moofileview/moofileview.h"
+
+G_BEGIN_DECLS
+
+
+
+G_END_DECLS
+
+#endif /* __MOO_FILE_VIEW_TOOLS_H__ */
