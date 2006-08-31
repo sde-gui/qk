7f58d29192274ae816d52b1c95ffc4937c994cf8
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2006-08-31 10:10:57 -0500

    Changed license to LGPL

--- moo/moopython/plugins/pyconsole.py
+++ moo/moopython/plugins/pyconsole.py
@@ -6,11 +6,11 @@
 #   Portions of code by Geoffrey French.
 #
 #   This program is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation; either version 2 of the License, or
-#   (at your option) any later version.
+#   it under the terms of the GNU Lesser General Public version 2.1 as
+#   published by the Free Software Foundation.
 #
-#   See COPYING file that comes with this distribution.
+#   See COPYING.lib file that comes with this distribution for full text
+#   of the license.
 #
 
 # This module 'runs' python interpreter in a TextView widget.
