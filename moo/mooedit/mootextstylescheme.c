17734d7dea11b4240066113de092dcb72b0b315a
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2007-04-07 03:21:52 -0500

    Updated copyright notices

--- moo/mooedit/mootextstylescheme.c
+++ moo/mooedit/mootextstylescheme.c
@@ -1,7 +1,7 @@
 /*
  *   mootextstylescheme.c
  *
- *   Copyright (C) 2004-2006 by Yevgen Muntyan <muntyan@math.tamu.edu>
+ *   Copyright (C) 2004-2007 by Yevgen Muntyan <muntyan@math.tamu.edu>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
