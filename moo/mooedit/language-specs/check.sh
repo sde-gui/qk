79187ac574b31706f5e8e5d65765941b2dec7e46
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2007-06-24 17:12:43 -0500

    Removed headers from C++ mime types and globs

--- moo/mooedit/language-specs/check.sh
+++ moo/mooedit/language-specs/check.sh
@@ -7,7 +7,7 @@ langs="ada.lang changelog.lang chdr.lang c.lang cpp.lang csharp.lang
        css.lang def.lang desktop.lang diff.lang dpatch.lang dtd.lang
        fortran.lang gap.lang gtk-doc.lang gtkrc.lang haskell.lang
        html.lang idl.lang ini.lang java.lang javascript.lang latex.lang
-       libtool.lang m4.lang makefile.lang ms.lang objc.lang ocaml.lang
+       libtool.lang m4.lang makefile.lang objc.lang ocaml.lang
        octave.lang pascal.lang perl.lang php.lang pkgconfig.lang po.lang
        python.lang ruby.lang scheme.lang sh.lang sql.lang tcl.lang
        texinfo.lang verilog.lang xml.lang yacc.lang"
