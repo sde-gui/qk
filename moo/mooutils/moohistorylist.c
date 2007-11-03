87d20b4702903e6c64d616f32de994bcf6a6e4bc
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2007-11-03 14:12:35 -0500

    Do not hash NULL

--- moo/mooutils/moohistorylist.c
+++ moo/mooutils/moohistorylist.c
@@ -686,7 +686,8 @@ moo_history_list_new (const char *user_id)
 
     list = g_object_new (MOO_TYPE_HISTORY_LIST,
                          "user-id", user_id, NULL);
-    add_named_list (user_id, list);
+    if (user_id)
+        add_named_list (user_id, list);
 
     return list;
 }
