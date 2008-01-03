2a30d62a190300161a92c707a2a365b9a763d59e
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2008-01-03 14:52:24 -0600

    Initialize ctags plugin after file selector and file list

--- moo/mooedit/mooplugin.c
+++ moo/mooedit/mooplugin.c
@@ -908,14 +908,14 @@ moo_plugin_get_dirs (void)
 static void
 moo_plugin_init_builtin (void)
 {
+    _moo_file_selector_plugin_init ();
+    _moo_file_list_plugin_init ();
 #ifndef __WIN32__
 #ifdef MOO_BUILD_CTAGS
     _moo_ctags_plugin_init ();
 #endif
     _moo_find_plugin_init ();
 #endif
-    _moo_file_selector_plugin_init ();
-    _moo_file_list_plugin_init ();
 #if 0
     _moo_completion_plugin_init ();
     _moo_active_strings_plugin_init ();
