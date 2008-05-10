c4e7db9cfd69d4da225c8979f43d787036ae8dfb
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2008-05-10 14:24:44 -0500

    Unset $LANG

--- medit/mac/environment.sh
+++ medit/mac/environment.sh
@@ -1,6 +1,9 @@
 # export EXTRA_ARGS="--project-mode"
 # export GTK2_RC_FILES="$GTK2_RC_FILES:$bundle_etc/gtkrc.medit"
 
+# ige-mac-bundler sets it to some garbage, unset it
+unset LANG
+
 export PYTHONHOME="$bundle_res"
 export PATH="$bundle_bin:$PATH"
 
