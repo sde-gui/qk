2bb10e6c89093972890e89ab0397dad3aaf11d01
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2008-08-18 06:46:35 -0500

    Fixed for relative srcdir paths

--- ugly/repo/ugly-post.mk
+++ ugly/repo/ugly-post.mk
@@ -2,8 +2,9 @@
 # -%- lang: makefile; indent-width: 8; use-tabs: true -%-
 
 $(srcdir)/Makefile.am: $(srcdir)/Makefile.ug $(UGLY_DEPS)
-	@echo 'cd $(srcdir) && $(UGLY) $(top_srcdir) $(subdir)'; \
-	cd $(srcdir) && $(UGLY) $(top_srcdir) $(subdir) && exit 0; \
+	@top_srcdir=`cd $(top_srcdir) && pwd`; \
+	echo 'cd $(srcdir) && $$top_srcdir/ugly/ugly $$top_srcdir $(subdir)'; \
+	cd $(srcdir) && $$top_srcdir/ugly/ugly $$top_srcdir $(subdir) && exit 0; \
 	exit 1;
 
 # end ugly-post.mk
