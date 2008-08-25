52af70df854dedb340bfde4ef7c3bfdc30b28d80
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2008-08-25 04:50:41 -0500

    Link to libtool libmoo.la when that's available, and to the static libs and gtk libs otherwise

--- moo/moo-comps.mk
+++ moo/moo-comps.mk
@@ -1,41 +1,34 @@
 # -%- lang: makefile; indent-width: 8; use-tabs: true -%-
 
-moo_sublibs =
+moo_app_ldadd =
+
+if MOO_INSTALL_LIB
+
+moo_app_ldadd += $(top_builddir)/moo/libmoo.la
+
+else
 
 if MOO_PYTHON_BUILTIN
-moo_sublibs += $(top_builddir)/moo/moopython/libmoopython.la
+moo_app_ldadd += $(top_builddir)/moo/moopython/libmoopython.la
 endif
 
 if MOO_BUILD_APP
-if MOO_INSTALL_LIB
-moo_sublibs += $(top_builddir)/moo/mooapp/libmooapp.la
-else
-moo_sublibs += $(top_builddir)/moo/mooapp/libmooapp.a
-endif
+moo_app_ldadd += $(top_builddir)/moo/mooapp/libmooapp.a
 endif
 
 if MOO_BUILD_EDIT
-if MOO_INSTALL_LIB
-moo_sublibs += $(top_builddir)/moo/mooedit/libmooedit.la
-moo_sublibs += $(top_builddir)/moo/moofileview/libmoofileview.la
-else
-moo_sublibs += $(top_builddir)/moo/mooedit/libmooedit.a
-moo_sublibs += $(top_builddir)/moo/moofileview/libmoofileview.a
-endif
+moo_app_ldadd += $(top_builddir)/moo/mooedit/libmooedit.a
+moo_app_ldadd += $(top_builddir)/moo/moofileview/libmoofileview.a
 endif
 
 if MOO_BUILD_LUA
-if MOO_INSTALL_LIB
-moo_sublibs += $(top_builddir)/moo/moolua/libmoolua.la
-else
-moo_sublibs += $(top_builddir)/moo/moolua/libmoolua.a
-endif
+moo_app_ldadd += $(top_builddir)/moo/moolua/libmoolua.a
 endif
 
 if MOO_BUILD_UTILS
-if MOO_INSTALL_LIB
-moo_sublibs += $(top_builddir)/moo/mooutils/libmooutils.la
-else
-moo_sublibs += $(top_builddir)/moo/mooutils/libmooutils.a
+moo_app_ldadd += $(top_builddir)/moo/mooutils/libmooutils.a
 endif
+
+moo_app_ldadd += $(MOO_LIBS)
+
 endif
