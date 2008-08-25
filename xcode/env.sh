561adacdb810a0ddbcecfa351b4614805084fe38
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2008-08-25 04:54:04 -0500

    Removed obsolete projects

--- xcode/env.sh
+++
@@ -1,2 +0,0 @@
-export MOO_SRCDIR="`cd ../moo && pwd`"
-export PATH=/opt/gtk/bin:/usr/local/bin:$PATH
--- xcode/moo-config-dirs.h
+++
@@ -1,4 +0,0 @@
-#define MOO_DATA_DIR "/opt/gtk/share/moo"
-#define MOO_LIB_DIR "/opt/gtk/lib/moo"
-#define MOO_LOCALE_DIR "/opt/gtk/share/locale"
-#define MOO_HELP_DIR "/opt/gtk/share/doc/medit/help"
