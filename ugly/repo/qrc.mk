6814433e96c97d04985ae900f41c5c64b7813f0d
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2008-09-07 08:51:25 -0500

    Donot distriuted generated -qrc.cpp files

--- ugly/repo/qrc.mk
+++ ugly/repo/qrc.mk
@@ -17,4 +17,5 @@ $(ugly_qrc_cpp): $(QT_QRC_FILES) $(QT_QRC_DEPS)
 
 EXTRA_DIST += $(QT_QRC_FILES) $(QT_QRC_DEPS)
 BUILT_SOURCES += $(ugly_qrc_cpp)
-@MODULE@_SOURCES += $(ugly_qrc_cpp)
+CLEANFILES += $(ugly_qrc_cpp)
+nodist_@MODULE@_SOURCES += $(ugly_qrc_cpp)
