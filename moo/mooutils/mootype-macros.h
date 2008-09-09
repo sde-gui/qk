a0251dea52052a9143dab750ec2a2442dcc50a1c
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2008-09-09 17:08:17 -0500

    Thread-safe type registration

--- moo/mooutils/mootype-macros.h
+++ moo/mooutils/mootype-macros.h
@@ -65,16 +65,18 @@ static void     type_name##_class_intern_init (gpointer klass)                  
 static GType                                                                                \
 type_name##_get_type (void)                                                                 \
 {                                                                                           \
-    static GType g_define_type_id;                                                          \
+    static volatile gsize g_define_type_id__volatile;                                       \
                                                                                             \
-    if (G_UNLIKELY (!g_define_type_id))                                                     \
+    if (g_once_init_enter (&g_define_type_id__volatile))                                    \
     {                                                                                       \
+        GType g_define_type_id;                                                             \
         _MOO_REGISTER_TYPE(TypeName,type_name,TYPE_PARENT,0)                                \
         code                                                                                \
+        g_once_init_leave (&g_define_type_id__volatile, g_define_type_id);                  \
     }                                                                                       \
                                                                                             \
-    return g_define_type_id;                                                                \
-} /* closes type_name##_get_type() */
+    return g_define_type_id__volatile;                                                      \
+}
 
 #define MOO_DEFINE_TYPE_STATIC(TypeName,type_name,TYPE_PARENT)                              \
     MOO_DEFINE_TYPE_STATIC_WITH_CODE (TypeName, type_name, TYPE_PARENT, {})
@@ -82,15 +84,18 @@ type_name##_get_type (void)                                                     
 
 #define MOO_DEFINE_BOXED_TYPE__(TypeName,type_name,copy_func,free_func)                     \
 {                                                                                           \
-    static GType g_define_type_id;                                                          \
+    static volatile gsize g_define_type_id__volatile;                                       \
                                                                                             \
-    if (G_UNLIKELY (!g_define_type_id))                                                     \
-        g_define_type_id =                                                                  \
+    if (g_once_init_enter (&g_define_type_id__volatile))                                    \
+    {                                                                                       \
+        GType g_define_type_id =                                                            \
             g_boxed_type_register_static (#TypeName,                                        \
                                           (GBoxedCopyFunc) copy_func,                       \
                                           (GBoxedFreeFunc) free_func);                      \
+        g_once_init_leave (&g_define_type_id__volatile, g_define_type_id);                  \
+    }                                                                                       \
                                                                                             \
-    return g_define_type_id;                                                                \
+    return g_define_type_id__volatile;                                                      \
 }
 
 #define MOO_DEFINE_BOXED_TYPE(TypeName,type_name,copy_func,free_func)                       \
@@ -118,24 +123,29 @@ static GType type_name##_get_type (void)                                        
 #define MOO_DEFINE_POINTER_TYPE(TypeName,type_name)                                         \
 GType type_name##_get_type (void)                                                           \
 {                                                                                           \
-    static GType g_define_type_id;                                                          \
+    static volatile gsize g_define_type_id__volatile;                                       \
                                                                                             \
-    if (G_UNLIKELY (!g_define_type_id))                                                     \
-        g_define_type_id =                                                                  \
-            g_pointer_type_register_static (#TypeName);                                     \
+    if (g_once_init_enter (&g_define_type_id__volatile))                                    \
+    {                                                                                       \
+        GType g_define_type_id = g_pointer_type_register_static (#TypeName);                \
+        g_once_init_leave (&g_define_type_id__volatile, g_define_type_id);                  \
+    }                                                                                       \
                                                                                             \
-    return g_define_type_id;                                                                \
+    return g_define_type_id__volatile;                                                      \
 }
 
 
 #define MOO_DEFINE_QUARK__(QuarkName)                                                       \
 {                                                                                           \
-    static GQuark q;                                                                        \
+    static volatile gsize q_volatile;                                                       \
                                                                                             \
-    if (G_UNLIKELY (!q))                                                                    \
-        q = g_quark_from_static_string (#QuarkName);                                        \
+    if (g_once_init_enter (&q_volatile))                                                    \
+    {                                                                                       \
+        GQuark q = g_quark_from_static_string (#QuarkName);                                 \
+        g_once_init_leave (&q_volatile, q);                                                 \
+    }                                                                                       \
                                                                                             \
-    return q;                                                                               \
+    return q_volatile;                                                                      \
 }
 
 #define MOO_DEFINE_QUARK(QuarkName,quark_func)                                              \
