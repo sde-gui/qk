f6dfc964b94780aa59ea6333c91df8c78a1885d2
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2010-02-28 00:26:39 -0800

    Added back intltool stuff

--- cmake/modules/FindMooI18n.cmake
+++ cmake/modules/FindMooI18n.cmake
@@ -24,6 +24,49 @@ MACRO(MOO_ADD_MSG_CATALOG catalogname dir)
       INSTALL(FILES ${_moo_gmo_file} DESTINATION ${LOCALEDIR}/${_moo_lang_code}/LC_MESSAGES/ RENAME ${catalogname}.mo)
     ENDFOREACH(_moo_po_file)
     SET_SOURCE_FILES_PROPERTIES(${_moo_gmo_files} PROPERTIES GENERATED 1 EXTERNAL_OBJECT 1)
-    ADD_CUSTOM_TARGET(translations-${catalogname} ALL SOURCES ${_moo_gmo_files})
+    ADD_CUSTOM_TARGET(intl-${catalogname} ALL SOURCES ${_moo_gmo_files})
   ENDIF(ENABLE_NLS)
 ENDMACRO(MOO_ADD_MSG_CATALOG)
+
+IF(ENABLE_NLS)
+  IF(WIN32)
+    MACRO(_MOO_FIND_INTLTOOL_VAR var script)
+      SET(${var} $ENV{${var}})
+      IF(NOT "${var}")
+        FIND_FILE(${var} ${script})
+      ENDIF(NOT "${var}")
+    ENDMACRO(_MOO_FIND_INTLTOOL_VAR)
+    _MOO_FIND_INTLTOOL_VAR(INTLTOOL_UPDATE intltool-update)
+    _MOO_FIND_INTLTOOL_VAR(INTLTOOL_EXTRACT intltool-extract)
+    _MOO_FIND_INTLTOOL_VAR(INTLTOOL_MERGE intltool-merge)
+    CONFIGURE_FILE(${MOO_SOURCE_DIR}/plat/win32/intltool-wrapper.bat.in ${MOO_BINARY_DIR}/intltool-wrapper.bat)
+    SET(INTLTOOL_UPDATE_COMMAND ${MOO_BINARY_DIR}/intltool-wrapper.bat)
+  ELSE(WIN32)
+    FIND_PROGRAM(INTLTOOL_UPDATE_COMMAND intltool-update)
+  ENDIF(WIN32)
+
+  SET(MOO_PO_DIR ${MOO_SOURCE_DIR}/po)
+
+  ADD_CUSTOM_TARGET(
+    intl-check
+    ${INTLTOOL_UPDATE_COMMAND} --gettext-package=moo -m
+    WORKING_DIRECTORY ${MOO_PO_DIR}
+  )
+
+  ADD_CUSTOM_TARGET(
+    intl-pot
+    COMMAND ${INTLTOOL_UPDATE_COMMAND} --gettext-package=moo -p
+    WORKING_DIRECTORY ${MOO_PO_DIR}
+  )
+
+  FILE(STRINGS ${MOO_PO_DIR}/LINGUAS MOO_PO_LINGUAS)
+  ADD_CUSTOM_TARGET(intl-update-po)
+  FOREACH(_moo_lang ${MOO_PO_LINGUAS})
+    ADD_CUSTOM_TARGET(
+      intl-update-${_moo_lang}-po
+      ${INTLTOOL_UPDATE_COMMAND} --gettext-package=moo -d ${_moo_lang}
+      WORKING_DIRECTORY ${MOO_PO_DIR}
+    )
+    ADD_DEPENDENCIES(intl-update-po intl-update-${_moo_lang}-po)
+  ENDFOREACH(_moo_lang)
+ENDIF(ENABLE_NLS)
