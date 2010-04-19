327e3c8a686404e95a0c92a108ce4438a37c7c9a
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2010-04-19 02:32:31 -0700

    Make glib-win32.cache read-only

--- plat/win32/gtk-win/mjhbuild.sh
+++ plat/win32/gtk-win/mjhbuild.sh
@@ -6,5 +6,7 @@ export mgwdir=`dirname $0`
 export _GTKWINDIR=`cd $mgwdir && pwd`
 export _GTKWINCONFIG=$mgwconfig
 
+chmod a-w $mgwdir/jhbuildrc/glib-win32.cache
+
 echo $mgwdir_s/jhbuild/bin/jhbuild -f $mgwdir_s/jhbuildrc/jhbuildrc "$@"
 exec $mgwdir_s/jhbuild/bin/jhbuild -f $mgwdir_s/jhbuildrc/jhbuildrc "$@"
