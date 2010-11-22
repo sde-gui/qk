d3e7f6bbfd6135f32dc272e13c465560659d3b21
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2010-11-21 20:29:55 -0800

    Removing junk

--- moo/mooedit/junk/mootextview.h
+++
@@ -1,5 +0,0 @@
-void         moo_text_view_insert_placeholder       (MooTextView        *view,
-                                                     GtkTextIter        *iter,
-                                                     const char         *text);
-gboolean     moo_text_view_next_placeholder         (MooTextView        *view);
-gboolean     moo_text_view_prev_placeholder         (MooTextView        *view);
