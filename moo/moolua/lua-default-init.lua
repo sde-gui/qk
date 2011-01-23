9bca0625ae4249acd043174066466a46bcc53a26
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2011-01-22 22:23:29 -0800

    Do not fail horribly if _moo.* packages are not found, just print an error message

--- moo/moolua/lua-default-init.lua
+++ moo/moolua/lua-default-init.lua
@@ -1,5 +1,15 @@
 local _g = getfenv(0)
-require("_moo.builtin")._inject(_g)
+
+do
+  local status, result
+  status, result = pcall(require, "_moo.builtin")
+  if status then
+    result._inject(_g)
+  else
+    print(result)
+  end
+end
+
 local moo = require("moo")
 _g.app = moo.App.instance()
 _g.editor = _g.app.get_editor()
