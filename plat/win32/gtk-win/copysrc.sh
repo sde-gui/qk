784f44382d1a06d83476306e5339a13a478e8dcd
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2011-12-05 02:41:07 -0800

    Pack all source tarballs into one

--- plat/win32/gtk-win/copysrc.sh
+++ plat/win32/gtk-win/copysrc.sh
@@ -5,7 +5,8 @@ export mgwdir=`dirname $0`
 
 jhtarballdir=$mgwbuildroot/tarballs
 jhsourcedir=$mgwbuilddir/source
-destdir=$mgwbuildroot/medit-deps-src-`date +%Y%m%d`
+destname=medit-deps-src-`date +%Y%m%d`
+destdir=$mgwbuildroot/$destname
 
 do_or_die() {
   echo "$@"
@@ -14,11 +15,7 @@ do_or_die() {
 
 cd $mgwbuildroot
 
-if [ -e $destdir ]; then
-  echo "Directory $destdir already exists"
-  exit 1
-fi
-
+do_or_die rm -fr $destdir $destdir.tar.bz2
 do_or_die mkdir $destdir
 do_or_die cp -lfR $jhtarballdir/* $destdir/
 
@@ -30,11 +27,10 @@ for proj in pycairo pygobject pygtk; do
     mv medit-$proj.tar.bz2 ..
 done
 do_or_die cd $destdir
-do_or_die rm -r $destdir/tmp
+do_or_die rm -fr $destdir/tmp
 do_or_die cp -lfR $mgwdir $destdir/gtk-win
 do_or_die mv $destdir/gtk-win/extra/readme-medit-deps.txt $destdir/readme.txt
-do_or_die tar cjf gtk-win.tar.bz2 gtk-win
-do_or_die rm -fr gtk-win
 
-# cd $mgwbuildroot
-# do_or_die zip -r src-$mgwconfig.zip src-$mgwconfig
+do_or_die cd $destdir/..
+do_or_die tar cjf $destname.tar.bz2 $destname
+do_or_die rm -fr $destname
