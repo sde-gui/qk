1c41de3ea22e1d32c1a879c6ddac23c0c125f364
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2012-07-29 06:12:48 +0100

    Make gtk-win build work on cygwin

--- plat/win32/gtk-win/config.sh
+++ plat/win32/gtk-win/config.sh
@@ -1,9 +1,28 @@
+if [ -z "$OSTYPE" -o "$OSTYPE" != "cygwin" ]; then
+  export mgwsystem=linux
+else
+  export mgwsystem=cygwin
+fi
+if [ $mgwsystem = linux ]; then
+  export mgwlinux=true
+  export mgwcygwin=false
+else
+  export mgwlinux=false
+  export mgwcygwin=true
+fi
+
 export mgwbuildroot=$HOME/projects/gtk-win-build
 export mgwpythondotver=2.7
 
 export mgwpythonver=`echo $mgwpythondotver | sed 's/[.]//'`
-export mgwpythoninstdir=$HOME/.wine/drive_c/Python$mgwpythonver
-export mgwpythonsystem32dir=$HOME/.wine/drive_c/windows/system32
+
+if $mgwlinux; then
+  export mgwpythoninstdir=$HOME/.wine/drive_c/Python$mgwpythonver
+  export mgwpythonsystem32dir=$HOME/.wine/drive_c/windows/system32
+else
+  export mgwpythoninstdir=/cygdrive/c/Tools/Python$mgwpythonver
+  export mgwpythonsystem32dir=/cygdrive/c/Windows/SysWOW64
+fi
 
 export mgwdir_s=$mgwdir
 export mgwdir=`cd $mgwdir && pwd`
