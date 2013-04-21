22f5f3c458729a051a402eddfe7f9881f05b30b9
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2013-04-20 22:31:16 -0700

    Fixed annotations on moo_ui_xml_add_item(), which broke simple.py; and fixed up simple.py as well, so it works with buggy versions of medit

--- moo/mooutils/moouixml.c
+++ moo/mooutils/moouixml.c
@@ -899,9 +899,9 @@ lookup_merge (MooUiXml *xml,
  * @xml:
  * @merge_id:
  * @parent_path: (type const-utf8)
- * @name: (type const-utf8)
+ * @name: (type const-utf8) (allow-none) (default NULL)
  * @action: (type const-utf8)
- * @position:
+ * @position: (type index) (default -1)
  */
 MooUiNode*
 moo_ui_xml_add_item (MooUiXml       *xml,
@@ -957,7 +957,7 @@ moo_ui_xml_add_item (MooUiXml       *xml,
  * @xml:
  * @merge_id:
  * @parent:
- * @position:
+ * @position: (type index) (default -1)
  * @markup: (type const-utf8)
  */
 void
@@ -1217,7 +1217,7 @@ moo_ui_xml_insert_markup_before (MooUiXml       *xml,
  * @xml:
  * @merge_id:
  * @parent_path: (type const-utf8)
- * @position:
+ * @position: (type index) (default -1)
  * @markup: (type const-utf8)
  */
 void
