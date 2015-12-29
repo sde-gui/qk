837787bdbc27d6c513795eaa0fa6c0242b6090a3
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2015-12-29 02:53:37 -0800

    Gtk bin dir moved

--- plat/win32/gtk-win/genlib.bat
+++ plat/win32/gtk-win/genlib.bat
@@ -3,7 +3,7 @@ setlocal
 
 call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat"
 
-cd C:\Projects\debian\gtk-win-build\release\target\lib
+cd C:\Projects\gtk\release\target\lib
 
 FOR %%d IN (*.def) DO (
   echo %%d
