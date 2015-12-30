e3349033e9adf1227d0109f5595a6f68e7a334dc
Yevgen Muntyan <emuntyan@users.sourceforge.net>
2015-12-29 19:10:39 -0800

    More cmake tweaks

--- moo/medit-app/medit-app.cmake
+++ moo/medit-app/medit-app.cmake
@@ -1,14 +1,16 @@
 set(medit_app_sources
+    medit-app/medit-app.cmake
     medit-app/mem-debug.h
     medit-app/run-tests.h
     medit-app/parse.h
     medit-app/main.c
-    medit-app/medit-app.cmake
 )
 
 set(built_medit_app_sources)
 
 if(MOO_OS_WIN32)
+    set(top_srcdir ${CMAKE_SOURCE_DIR})
     configure_file(${CMAKE_CURRENT_SOURCE_DIR}/medit-app/medit.rc.in ${CMAKE_CURRENT_BINARY_DIR}/medit-app/medit.rc)
     list(APPEND built_medit_app_sources ${CMAKE_CURRENT_BINARY_DIR}/medit-app/medit.rc)
+    list(APPEND medit_app_sources ${CMAKE_CURRENT_SOURCE_DIR}/medit-app/medit.rc.in)
 endif()
